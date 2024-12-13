cbd6be49839d74a35befe3bdc836d8c752e78cf8
Albertojs89 <jimenezsanchezalberto@fpllefia.com>
2024-12-13 14:53:49 +0100

    configInicial

---
+++ src/App.jsx
@@ -0,0 +1,32 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+  
+
+  return (
+    <>
+      <div>
+        <a href="" target="_blank">
+          <img
+            src="https://th.bing.com/th/id/R.1a56719bd55538b16725160d071acacb?rik=%2fd48wcHgiwnZSA&riu=http%3a%2f%2fwww.pngmart.com%2ffiles%2f13%2fTetris-Logo-PNG-Image.png&ehk=Fa1JlyhJXr4DxFChC6ovyCgUELzDMW04KXmTn8T4l24%3d&risl=&pid=ImgRaw&r=0"
+            className="logo"
+            alt="Vite logo"
+          />
+        </a>
+       
+      </div>
+      <h1>TETRIS</h1>
+      <h2>Alberto</h2>
+     
+      <p className="read-the-docs">
+        Click on the Vite and React logos to learn more
+      </p>
+      <button className="btn btn-primary">Botón de Bootstrap</button>
+    </>
+  );
+}
+
+export default App
