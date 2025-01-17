c73289118508965c9717782d2636135897fc5861
Albertojs89 <jimenezsanchezalberto@fpllefia.com>
2025-01-17 16:31:01 +0100

    tabla por retocar

--- src/App.jsx
+++ src/App.jsx
@@ -9,25 +9,7 @@ function App() {
 
   return (
     <>
-      <div>
-        <a href="" target="_blank">
-          <img
-            src="https://th.bing.com/th/id/R.1a56719bd55538b16725160d071acacb?rik=%2fd48wcHgiwnZSA&riu=http%3a%2f%2fwww.pngmart.com%2ffiles%2f13%2fTetris-Logo-PNG-Image.png&ehk=Fa1JlyhJXr4DxFChC6ovyCgUELzDMW04KXmTn8T4l24%3d&risl=&pid=ImgRaw&r=0"
-            className="logo"
-            alt="Vite logo"
-          />
-        </a>
-      </div>
-      <h1>
-        <a href="" className="logo">
-          TETRIS
-        </a>
-      </h1>
-      <h2>
-        <a href="" className="logo">
-          Alberto
-        </a>
-      </h2>
+      
 
       <div className="App">
         <Partidas />
